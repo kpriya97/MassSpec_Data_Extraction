98d9a0289578369aa41699b2228adbda09838344
rohitharavinder <rohitha0112@gmail.com>
2022-01-18 17:18:09 +0100

    Added decoding/decompression code

--- package/main_package/test_minidom.py
+++ package/main_package/test_minidom.py
@@ -1,5 +1,6 @@
 from xml.dom import minidom as md
 from typing import List, Dict
+import base64, zlib, struct
 
 
 def parse_file(path: str):
@@ -54,7 +55,7 @@ def get_spectrum_dict(spectrum_list: List) -> Dict:
     for spectrum in spectrum_list:
         ids = int(spectrum.getAttribute("index"))
         spectrum_dict[ids] = spectrum
-    print(spectrum_dict.keys())
+    # print(spectrum_dict.keys())
     return spectrum_dict
 
 
@@ -86,8 +87,34 @@ def get_spectrum_values(spectrum_dict: Dict) -> Dict[str, Dict]:
                              'intensity': vals[key]['intensity'].firstChild.nodeValue}
         else:
             bin_dict[key] = {'mz': None, 'intensity': None}
-    #del vals
     return bin_dict
+
+
+def decode_decompress(vals: dict) -> Dict:
+    """
+    Takes the raw spectrum values and returns a dictionary of decoded and uncompressed m/z and intensity values.
+    Parameters
+    ----------
+    vals: Dict
+        dictionary of raw spectrum values
+
+    Returns
+    -------
+    spectrum_data: Dict
+        dictionary of decoded and uncompressed m/z and intensity values
+    """
+    spectrum_data = dict()
+    for key in vals:
+        encoded_mz_data, encoded_int_data = vals[key]['mz'], vals[key]['intensity']
+        if encoded_mz_data is not None or encoded_int_data is not None:
+            decoded_mz_data, decoded_int_data = base64.standard_b64decode(encoded_mz_data), base64.standard_b64decode(encoded_int_data)  # # decodes the string
+            decompressed_mz_data, decompressed_int_data = zlib.decompress(decoded_mz_data), zlib.decompress(decoded_int_data)  # decompresses the data
+            mz_data = struct.unpack('<%sf' % (len(decompressed_mz_data) // 4), decompressed_mz_data)  # unpacks 32-bit m/z values as floats
+            int_data = struct.unpack('<%sf' % (len(decompressed_int_data) // 4), decompressed_int_data)  # unpacks 32-bit intensity values as floats
+            spectrum_data[key] = {'mz': mz_data, 'intensity': int_data}
+        else:
+            spectrum_data[key] = {'mz': None, 'intensity': None}
+    return spectrum_data
 
 
 def analyse_spectrum(path: str) -> Dict:
@@ -107,11 +134,9 @@ def analyse_spectrum(path: str) -> Dict:
     s_list = get_spectrum_list(p_file)
     spectrum_dictionary = get_spectrum_dict(s_list)
     vals = get_spectrum_values(spectrum_dictionary)
-    return vals
+    data = decode_decompress(vals)
+    # print(data)
+    return data
 
 
-values = analyse_spectrum('../tests/data/sample_ms_file1.mzML')
-print(values[0]['intensity'])
-
-
-
+analyse_spectrum("../tests/data/sample_ms_file1.mzML")
