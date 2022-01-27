8bffbef3ec7ee880d227e2851ce3c2feeea3e7db
ambasts0 <s0shamba@uni-bonn.de>
2022-01-27 16:01:35 +0100

    test skeleton for task 2

---
+++ package/main_package/task2_test.py
@@ -0,0 +1,20 @@
+from pyopenms_oop import PeptideSearch
+import pytest
+import pandas as pd
+
+class TestPeptideSearch:
+    '''
+    A test class which conducts pytests on PeptideSearch class.
+    '''
+
+    def test_get_peptide_identification_values(self):
+        '''Checks whether the PeptideSearch class has correct peptide values stored in dictionary'''
+        pass
+
+    def test_get_sequence(self):
+        '''Checks whether list of hit peptide sequences is correctly identified by algorithm '''
+        pass
+
+    def test_peptide_wrapper(self):
+        '''Checks the peptide values stored in dataframe'''
+        pass
